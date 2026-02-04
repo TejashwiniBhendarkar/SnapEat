59b2c0aa7042a0348e7cdce3e3c4dfaab34cd732
Tejashwini Bhendarkar <165932676+TejashwiniBhendarkar@users.noreply.github.com>
2026-02-04 13:54:08 +0530

    Refactor useGetCurrentUser hook for clarity and error handling

--- frontend/src/hooks/useGetCurrentUser.jsx
+++ frontend/src/hooks/useGetCurrentUser.jsx
@@ -1,11 +1,11 @@
-import axios from 'axios'
-import { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch } from 'react-redux'
-import { setUserData } from '../redux/userSlice'
+import axios from "axios";
+import { useEffect } from "react";
+import { serverUrl } from "../App";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../redux/userSlice";
 
 function useGetCurrentUser() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -13,43 +13,77 @@ function useGetCurrentUser() {
         const result = await axios.get(
           `${serverUrl}/api/user/current`,
           { withCredentials: true }
-        )
-        dispatch(setUserData(result.data))
+        );
+
+        dispatch(setUserData(result.data));
       } catch (error) {
-        // ✅ 401 is NORMAL if user is not logged in
-        if (error.response?.status !== 401) {
-          console.log(error)
+        // 🔥 IMPORTANT FIX
+        if (error.response?.status === 401) {
+          dispatch(setUserData(null));
+        } else {
+          console.error("getCurrentUser error:", error);
         }
       }
-    }
+    };
 
-    fetchUser()
-  }, [dispatch])
+    fetchUser();
+  }, [dispatch]);
 }
 
-export default useGetCurrentUser
+export default useGetCurrentUser;
 
 // import axios from 'axios'
-// import React, { useEffect } from 'react'
+// import { useEffect } from 'react'
 // import { serverUrl } from '../App'
 // import { useDispatch } from 'react-redux'
 // import { setUserData } from '../redux/userSlice'
 
 // function useGetCurrentUser() {
-//     const dispatch=useDispatch()
-//   useEffect(()=>{
-//   const fetchUser=async () => {
-//     try {
-//            const result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
-//             dispatch(setUserData(result.data))
-  
-//     } catch (error) {
-//         console.log(error)
+//   const dispatch = useDispatch()
+
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       try {
+//         const result = await axios.get(
+//           `${serverUrl}/api/user/current`,
+//           { withCredentials: true }
+//         )
+//         dispatch(setUserData(result.data))
+//       } catch (error) {
+//         // ✅ 401 is NORMAL if user is not logged in
+//         if (error.response?.status !== 401) {
+//           console.log(error)
+//         }
+//       }
 //     }
-// }
-// fetchUser()
- 
-//   },[])
+
+//     fetchUser()
+//   }, [dispatch])
 // }
 
 // export default useGetCurrentUser
+
+// // import axios from 'axios'
+// // import React, { useEffect } from 'react'
+// // import { serverUrl } from '../App'
+// // import { useDispatch } from 'react-redux'
+// // import { setUserData } from '../redux/userSlice'
+
+// // function useGetCurrentUser() {
+// //     const dispatch=useDispatch()
+// //   useEffect(()=>{
+// //   const fetchUser=async () => {
+// //     try {
+// //            const result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
+// //             dispatch(setUserData(result.data))
+  
+// //     } catch (error) {
+// //         console.log(error)
+// //     }
+// // }
+// // fetchUser()
+ 
+// //   },[])
+// // }
+
+// // export default useGetCurrentUser
